081524e3dc0d0bbaab7bc5f66305db8fcac8cac2
Bon Jenn <133067965+BonJenn@users.noreply.github.com>
2023-11-19 02:47:48 -0800

    Added comments, lanes, and rays. Still working on rays turning black when they extend road borders.

--- car.js
+++ car.js
@@ -11,12 +11,13 @@ class Car {
         this.friction = 0.05;
         this.angle = 0;
 
-
+        this.sensor= new Sensor(this);
         this.controls = new Controls(); // Use 'new' keyword
     }
 
-    update(){
+    update(roadBorders){
        this.#move();
+       this.sensor.update(roadBorders);
     }
 
     #move(){
@@ -72,5 +73,6 @@ class Car {
         ctx.fill();
 
         ctx.restore();
+        this.sensor.draw(ctx);
     }
 }
